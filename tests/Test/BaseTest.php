ffde2b44a72d3f2bc26fb6a623acc1dd2d30cadd
xiaolin <462441355@qq.com>
2019-02-21 15:31:58 +0800

    * 初始化

---
+++ tests/Test/BaseTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/5/31
+ * Time: 上午10:47
+ */
+
+namespace Tests\Test;
+
+use Tests\TestCase;
+
+class BaseTest extends TestCase
+{
+    public function testTable()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,17 @@
+<?php
+// +----------------------------------------------------------------------
+// | EnumException.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 xiaolin All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: xiaolin <462441355@qq.com> <https://github.com/missxiaolin>
+// +----------------------------------------------------------------------
+namespace Tests;
+use Lin\Swoole\Common\File\File;
+use Lin\Swoole\Common\Redis\Redis;
+use PHPUnit\Framework\TestCase as UnitTestCase;
+
+abstract class TestCase extends UnitTestCase
+{
+
+}
