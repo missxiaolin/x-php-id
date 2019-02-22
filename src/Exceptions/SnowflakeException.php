c7ac7e313942ff14fdebef94d435f16d828195f8
xiaolin <462441355@qq.com>
2019-02-22 19:03:31 +0800

    * 修改composer

---
+++ src/Exceptions/SnowflakeException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gb
+ * Date: 2019-02-22
+ * Time: 19:02
+ */
+
+namespace Lin\Snowflake\Exceptions;
+
+use Exception;
+
+class SnowflakeException extends Exception
+{
+
+}
