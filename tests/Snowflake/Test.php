a5d98a13b49b4ce792e35a3b770158555c30fe00
xiaolin <462441355@qq.com>
2019-04-08 10:08:19 +0800

    * 增加单元测试

---
+++ tests/Snowflake/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gb
+ * Date: 2019-04-08
+ * Time: 10:07
+ */
+
+namespace Tests\Snowflake;
+
+use Lin\Snowflake\Client;
+
+/**
+ * Class Test
+ * @package Tests\Snowflake
+ */
+class Test extends Client
+{
+
+}
--- tests/Test/BaseTest.php
+++ tests/Test/BaseTest.php
@@ -8,6 +8,7 @@
 
 namespace Tests\Test;
 
+use Tests\Snowflake\Test;
 use Tests\TestCase;
 
 class BaseTest extends TestCase
@@ -15,5 +16,19 @@ class BaseTest extends TestCase
     public function testTable()
     {
         $this->assertTrue(true);
+    }
+
+    public function testExample()
+    {
+        $id = Test::getInstance()->id(1, 1, intval(strtotime('2017-05-27')));
+        $this->assertEquals('52908628381697', $id);
+        $this->assertEquals(14, strlen($id));
+    }
+
+    public function testMax()
+    {
+        $id = Test::getInstance()->id(1, 1, intval(strtotime('9999-01-01')));
+        $this->assertEquals('1056492899794948097', $id);
+        $this->assertEquals(19, strlen($id));
     }
 }
